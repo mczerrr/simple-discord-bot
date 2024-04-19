61d16612b3b135979597aaf7bc650cf67b4bf5ed
mczerrr <114849893+mczerrr@users.noreply.github.com>
2024-04-19 18:39:25 +0700

    Update node.js

--- node.js
+++ node.js
@@ -13,6 +13,15 @@ Bot.on("message", (message)=>{
     if(message.content == ".halloo"){ //If you type .test on your channel it will reply "Test".
         message.reply("Hiiiii")
     }
+    if(message.content == ".halloo"){ //If you type .test on your channel it will reply "Test".
+        message.reply("Hiiiii")
+    }
+    if(message.content == ".halloo"){ //If you type .test on your channel it will reply "Test".
+        message.reply("Hiiiii")
+    }
+    if(message.content == ".halloo"){ //If you type .test on your channel it will reply "Test".
+        message.reply("Hiiiii")
+    }
 })
 
 //Login
